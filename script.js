f609b57d1e5fc6952dcdc8c45e0fadd64b2a6e71
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-29 14:52:18 +0100

    Prima parte completata

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
 $(document).ready(function(){
 
+  // Creare un oggetto che descriva uno studente con le
+  // seguenti proprietà: nome, cognome e età. Stampare a
+  // schermo attraverso il for in tutte le proprietà
+
+  var studente = {
+    "nome" : "Guy",
+    "cognome" : "Eboulet",
+    "eta" : "32",
+  };
+
+  for (var key in studente) {
+    console.log(studente[key]);
+  }
+
+  // Creare un array di oggetti di studenti. Ciclare su tutti gli
+  // studenti e stampare per ognuno nome e cognome
+
+  var studenti = [
+    {
+      "nome" : "Guy",
+      "cognome" : "Eboulet",
+      "eta" : "32",
+    },
+    {
+      "nome" : "Anna",
+      "cognome" : "Barbieri",
+      "eta" : "30",
+    },
+    {
+      "nome" : "Jessica",
+      "cognome" : "Defusco",
+      "eta" : "26",
+    },
+    {
+      "nome" : "Giacomo",
+      "cognome" : "Anselmi",
+      "eta" : "25",
+    },
+    {
+      "nome" : "Enzo",
+      "cognome" : "Panni",
+      "eta" : "32",
+    },
+  ]
+
+  for (var i = 0; i < studenti.length; i++) {
+    var result = studenti[i];
+    console.log(result);
+  }
+
+
+
 });
