ccadbc5efc86bd37c6dfc3ddb97ffa50ed29446f
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-30 09:30:01 +0100

    Prova handlebars

--- esercizio_2/script.js
+++ esercizio_2/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
 
+  var context = {
+    title: "My New Post",
+    body: "This is my first post!",
+    author: "Jade"
+   };
+
+  var html = template(context);
+
+  $("#app").append(html);
 
 });
